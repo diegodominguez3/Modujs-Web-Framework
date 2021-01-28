648c010fe11786802454eed557a098d73a50f0a8
Diego Dominguez <diegodominguez333@gmail.com>
2021-01-27 20:59:25 -0700

    View Classes and building test user views

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
-const collection = User.buildUserCollection();
-
-collection.on('change', () => {
-  console.log(collection);
-});
-
-collection.fetch();
+const user = User.buildUser({ name: 'Diego', age: 24 });
+const root = document.getElementById('root');
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+} else {
+  throw new Error('root element not found');
+}
